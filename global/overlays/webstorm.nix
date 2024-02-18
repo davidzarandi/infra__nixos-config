c6a10b2554b616fd4d998f6d41fd8850fe1b1df7
Dávid J. Zarándi <noreply@zdaav.eu>
2024-02-18 11:56:59 +0100

    fix: webstorm download url

--- global/overlays/webstorm.nix
+++ global/overlays/webstorm.nix
@@ -2,7 +2,7 @@ final: super: {
   webstorm = super.jetbrains.webstorm.overrideAttrs (_: rec {
     version = "2023.3.2";
     src = builtins.fetchTarball {
-      url = "https://download.jetbrains.com/webstorm.nix/WebStorm-2023.3.2.tar.gz";
+      url = "https://download.jetbrains.com/webstorm/WebStorm-2023.3.2.tar.gz";
       sha256 = "1y32zzx4p2svw98402a68zpdvqq2gdciaz9xgv7pkk71iwn6v5yf";
     };
   });
